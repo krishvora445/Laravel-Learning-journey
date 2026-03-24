16c3d56f3b98790faab88dba235fb203e8281fc9
krish <vora76637@gmail.com>
2026-03-24 17:10:21 +0530

    Routing 101

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,12 @@ Route::get('/', function () {
 Route::get('/krish', function () {
     return view('krish');
 });
+Route::get('/about', function () {
+    return view('about');
+//     return 'About us';// Most of the time I didn't want to use this, but there are certain situations where I use.
+});
+Route::get('/contact', function () {
+    return view('contact');
+//     return 'About us';// Most of the time I didn't want to use this, but there are certain situations where I use.
+});
+
