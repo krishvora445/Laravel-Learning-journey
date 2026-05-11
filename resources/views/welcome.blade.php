277b9208779f8915e96d7edc102ac3bede3953d3
krish <vora76637@gmail.com>
2026-05-11 08:06:47 +0530

    Pass data to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="Home">
-<!--      <h1>{{ $greeting }}, {{ $person }}!👍</h1> -->
-    <!-- <h1>{{ $greeting }},  <?php echo htmlspecialchars($person) ?> !👍</h1> -->
+<!--<h1>{{ $greeting }}, {{ $person }}!👍</h1> -->
+    <!-- <h1>{{ $greeting }}  <?php echo htmlspecialchars($person) ?> 👍</h1> -->
     <x-card class="max-w-400">
-    <h1>{{ $greeting }}, {!! $person !!}!👍</h1> <!--//with our any  This will load whatever the person hold. Like if it's hold person keyword hold any script, it will just load the script and end up running script in your app.If you don't want to, you have to like add htmlspecialchars . -->
+    <h1>{{ $greeting }} {{ $person }}   !👍</h1> <!--//with our any  This will load whatever the person hold. Like if it's hold person keyword hold any script, it will just load the script and end up running script in your app.If you don't want to, you have to like add htmlspecialchars . -->
     </x-card>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome',[
-    'greeting' => 'Hello',
-    'person' => request('person'),
+    'greeting' => 'Hello,',
+    'person' => request('person','world'),
 ]);
 });
 
