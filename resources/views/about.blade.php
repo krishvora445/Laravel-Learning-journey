1d2957678b431a9b1c1ea9947d0b278f48ae42e3
krish <vora76637@gmail.com>
2026-05-11 17:34:15 +0530

    Forms

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout title="About Us">
-    <h1>About Us</h1>
-    <x-card class="max-w-400">
-    <p>We are a passionate team dedicated to providing the best services to our customers. Our mission is to create innovative solutions that make a difference in people's lives.</p>
-    </x-card>
-</x-layout>
