4f3ad9272ec8833765f8b61e488569b7e74cd902
krish <vora76637@gmail.com>
2026-05-28 17:17:11 +0530

    when to queue it up

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 
 
 <!DOCTYPE html>
-<html lang="en" data-theme="dracula">
+<html lang="en" data-theme="black">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
